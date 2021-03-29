0121ed8250de209439bc22067456c5e6120488bf
abdulrehman ajmal <abdulrehman.ajmal@outlook.com>
2021-03-30 00:38:00 +0500

    hist

---
+++ histogram.py
@@ -0,0 +1,14 @@
+import cv2 as cv
+import matplotlib.pyplot as plt
+CATS =cv.imread('Photos/cats 2.jpg')
+GRAY = cv.cvtColor(CATS,cv.COLOR_BGR2GRAY)
+GRAY_HIST= cv.calcHist([CATS],[2],None,[256],[0,255])
+plt.figure()
+plt.title('HISTOGRAM')
+plt.xlabel('BINS')
+plt.ylabel('# of pixels')
+plt.plot(GRAY_HIST)
+plt.xlim([0,256])
+plt.show()
+cv.imshow('CATS',CATS)
+cv.waitKey(0)
