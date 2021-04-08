7100758a27a6b5ee71501249ee052de98e2b4127
abdulrehman ajmal <abdulrehman.ajmal@outlook.com>
2021-04-08 19:54:46 +0500

    refactor

--- face_recognition.py
+++ face_recognition.py
@@ -1,12 +1,10 @@
 import cv2 as cv
-
-HARR_CASSCADE = cv.CascadeClassifier('harr.xml')
+HARR_CASSCADE = cv.CascadeClassifier('harr_face.xml')
 PEOPLE = ['Ben Afflek', 'Elton John','Jerry Seinfield', 'Madonna', 'Mindy Kaling']
 
 face_recogniser = cv.face.LBPHFaceRecognizer_create()
 face_recogniser.read('face_trained.yml')
-image = cv.imread('Photos/lady.jpg')
-cv.imshow('gray',image)
+image = cv.imread(r'Faces\val\ben_afflek\5.jpg')
 gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
 face_rect = HARR_CASSCADE.detectMultiScale(gray, 1.1, 4)
@@ -15,8 +13,8 @@ for (x,y,w,z) in face_rect:
     
     lable , confidence = face_recogniser.predict(face_roi)
     print(confidence)
-    cv.putText(image, str(PEOPLE[lable]), (x,z),cv.FONT_HERSHEY_PLAIN,1.0,(0,255,0),thickness=2)
-    cv.rectangle(image,(x,y),(x+w,y+z),(0,225,0),thickness=2)
+    cv.putText(image, str(PEOPLE[lable]), (x,-y+z-10),cv.FONT_HERSHEY_PLAIN,1.0,(0,255,0),thickness=1)
+    cv.rectangle(image,(x,y),(x+w,y+z),(0,225,0),thickness=1)
     
 cv.imshow('RECOGNIZER',image)
 cv.waitKey(0)
